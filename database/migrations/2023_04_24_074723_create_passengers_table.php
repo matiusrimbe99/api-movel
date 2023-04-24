933e9fd6d04efb0769354b7d3822a76c74788705
matiusrimbe99 <matiusrimbe.ptik@gmail.com>
2023-04-24 15:41:29 +0700

    create table and conn database

---
+++ database/migrations/2023_04_24_074723_create_passengers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->id();
+            $table->string('profile_picture')->nullable();
+            $table->string('home_address')->nullable();
+            $table->string('work_address')->nullable();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passengers');
+    }
+};
